4ec463b7c4604ecd2bf1db85a95c6f0be13653bd
角川拓夢 <sumikawa.web@gmail.com>
2020-02-16 19:47:28 +0900

    新規作成

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass'); //Sassコンパイル
+var plumber = require('gulp-plumber'); //エラー時の強制終了を防止
+var notify = require('gulp-notify'); //エラー発生時にデスクトップ通知する
+var sassGlob = require('gulp-sass-glob'); //@importの記述を簡潔にする
+var browserSync = require( 'browser-sync' ); //ブラウザ反映
+var postcss = require('gulp-postcss'); //autoprefixerとセット
+var autoprefixer = require('autoprefixer'); //ベンダープレフィックス付与
+var cssdeclsort = require('css-declaration-sorter'); //cssソート
+
+// scssのコンパイル
+gulp.task('sass', function() {
+return gulp
+.src( './src/assets/css/*.scss' )
+.pipe( plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }) )//エラーチェック
+.pipe( sassGlob() )//importの読み込みを簡潔にする
+.pipe( sass({
+outputStyle: 'expanded' //expanded, nested, campact, compressedから選択
+}) )
+.pipe( postcss([ autoprefixer(
+{
+// ☆IEは11以上、Androidは4.4以上
+// その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
+cascade: false}
+) ]) )
+.pipe( postcss([ cssdeclsort({ order: 'smacss' }) ]) )//プロパティをソートし直す(smacss順)
+.pipe(gulp.dest('./src/assets/css'));//コンパイル後の出力先
+});
+
+// 保存時のリロード
+gulp.task( 'browser-sync', function(done) {
+browserSync.init({
+
+//ローカル開発
+server: {
+baseDir: "src",
+index: "index.html"
+}
+});
+done();
+});
+
+gulp.task( 'bs-reload', function(done) {
+browserSync.reload();
+done();
+});
+
+// 監視
+gulp.task( 'watch', function(done) {
+gulp.watch( './src/assets/css/*.scss', gulp.task('sass') ); //sassが更新されたらgulp sassを実行
+gulp.watch('./src/assets/css/*.scss', gulp.task('bs-reload')); //sassが更新されたらbs-reloadを実行
+gulp.watch( './src/js/*.js', gulp.task('bs-reload') ); //jsが更新されたらbs-relaodを実行
+gulp.watch('./src/*.html', gulp.task('bs-reload')); //htmlが更新されたらbs-reloadを実行
+});
+
+// default
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', 'watch')));
