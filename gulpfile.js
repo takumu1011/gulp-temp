5fb0773590812dc0b9fe7d387ab764106f5572c3
tkm <sumikawa.web@gmail.com>
2020-06-26 20:31:38 +0900

    ajust gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,84 @@
-const {src, dest, watch, series, parallel} = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const loadPlugins = require('gulp-load-plugins');
 const $ = loadPlugins();
-const browserSync = require( 'browser-sync' );
+const browserSync = require('browser-sync');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssdeclsort = require('css-declaration-sorter');
 
-//compSass
-function compSass() {
-    return src('src/assets/css/*.scss')
-        .pipe($.plumber({errorHandler: $.notify.onError("Error: <%= error.message %>")}))
-        .pipe($.sassGlob())
-        .pipe($.sass({outputStyle: 'expanded'}))
-        .pipe(postcss([autoprefixer({
-            browsers: ["last 2 versions", "ie >= 11", "Android >= 4"],
-            cascade: false
-        })]))
-        .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-        .pipe(dest('src/assets/css/'));
+const paths = {
+  dist: 'dist/',
+  src: 'src/',
+  css: 'src/assets/css/',
+  js: 'src/assets/js/',
+  img: 'src/assets/img/',
+};
+
+//sass
+function sass() {
+  return src(paths.css + '*.scss')
+    .pipe(
+      $.plumber({
+        errorHandler: $.notify.onError('Error: <%= error.message %>'),
+      })
+    )
+    .pipe($.sassGlob())
+    .pipe($.sass({ outputStyle: 'expanded' }))
+    .pipe(
+      postcss([
+        autoprefixer({
+          browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(postcss([cssdeclsort({ order: 'smacss' })]))
+    .pipe(dest(paths.css));
 }
 //babel
 function babel() {
-    return src('src/assets/js/main.js')
-    .pipe($.babel({
-        presets: ['@babel/preset-env']
-    }))
+  return src(paths.js + 'main.js')
+    .pipe(
+      $.babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
     .pipe($.rename('script.js'))
-    .pipe(dest('src/assets/js/'));
+    .pipe(dest(paths.js));
 }
-// serve
-function serve(done) {
-    browserSync.init({
-        server: {
-            baseDir: "src",
-            index: "index.html"
-        }
-    });
-    done();
+// server
+function server(done) {
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+      index: 'index.html',
+    },
+  });
+  done();
 }
 //reload
 function reload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 // watching
 function watching(done) {
-    watch('src/assets/css/*.scss', series(compSass, reload));
-    watch('src/assets/js/main.js', series(babel, reload));
-    watch('src/*.html', reload);
+  watch(paths.src + '*.html', reload);
+  watch(paths.css + '*.scss', series(sass, reload));
+  watch(paths.js + 'main.js', series(babel, reload));
 }
-//release
+//build
 function release(done) {
-    src('src/index.html')
-    .pipe(dest('dist/'));
+  src(paths.src + '**/**').pipe(dest(paths.dist));
 
-    src([
-        'src/assets/css/style.css',
-        'src/assets/css/default.css'
-    ])
-    .pipe(dest('dist/assets/css/'));
-
-    src('src/assets/js/script.js')
-    .pipe(dest('dist/assets/js/'));
-
-    src('src/assets/img/**')
-    .pipe($.imagemin())
-    .pipe(dest('dist/assets/img/'));
-    done();
+  src('src/assets/img/**').pipe($.imagemin()).pipe(dest('dist/assets/img/'));
+  done();
 }
 
-exports.sass = compSass;
+exports.sass = sass;
 exports.babel = babel;
-exports.serve = serve;
+exports.server = server;
 exports.reload = reload;
 exports.watching = watching;
-exports.default = parallel(watching , serve);
-exports.release = release;
+exports.default = parallel(watching, server);
+exports.build = build;
